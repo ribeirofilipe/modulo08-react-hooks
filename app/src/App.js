8e36a187cb964ebe0dd1bfeaa76722aca12288dc
Filipe de Oliveira Ribeiro <foribeiro@viceri.com.br>
2019-11-23 22:21:41 -0300

    inserido useMemo e useEffect do react-hooks

--- app/src/App.js
+++ app/src/App.js
@@ -1,19 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
 function App() {
-  const [ techs, setTechs ] = useState(['Javascript']);
+  const [ techs, setTechs ] = useState([]);
   const [ newTech, setNewTech ] = useState('');
 
-  function handleAdd() {
+  useEffect(() => {
+    const storageTechs = localStorage.getItem('techs');
+
+    if (storageTechs) {
+      setTechs(JSON.parse(storageTechs));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('techs', JSON.stringify(techs));
+  }, [techs])
+
+  const handleAdd = useCallback(() => {
     setTechs([...techs, newTech]);
     setNewTech('');
-  }
+  });
+
+  const techsSize = useMemo(() => techs.length, [techs]);
 
   return (
     <>
       {techs.map(tech => (
         <li key={tech}>{tech}</li>
       ))}
+      <p>Você possui {techsSize} tecnologia(s)</p>
       <input value={newTech} onChange={e => setNewTech(e.target.value)} type="text"/>
       <button type="button" onClick={handleAdd}>Adicionar</button>
     </>
